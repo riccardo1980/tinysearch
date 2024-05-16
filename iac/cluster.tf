69d92d6226a30ee3ffda8b4d2861583daba48144
Riccardo Zanella <riccardo.zanella@gmail.com>
2024-05-16 16:02:41 +0200

    adding single node elastic cluster

--- iac/cluster.tf
+++ iac/cluster.tf
@@ -1,15 +1,17 @@
 resource "minikube_cluster" "docker" {
-  driver       = "docker"
-  cluster_name = "terraform-provider-minikube-acc-docker"
-  nodes        = 3
+  driver         = "docker"
+  cluster_name   = "terraform-provider-minikube-acc-docker"
+  nodes          = 3
+  # apiserver_port = 8443
   addons = [
     "dashboard",
-    "default-storageclass",
+    "volumesnapshots",
     "ingress",
-    "storage-provisioner",
+    "csi-hostpath-driver",
     "metrics-server"
   ]
 }
+
 
 provider "kubernetes" {
   host = minikube_cluster.docker.host
@@ -17,4 +19,27 @@ provider "kubernetes" {
   client_certificate     = minikube_cluster.docker.client_certificate
   client_key             = minikube_cluster.docker.client_key
   cluster_ca_certificate = minikube_cluster.docker.cluster_ca_certificate
+}
+
+
+# Multi-node minikube clusters are having issues with PVC:
+# error: 
+#   Elastic: "failed to obtain node locks, tried [/usr/share/elasticsearch/data]; maybe these locations are not writable or multiple nodes were started on"
+# see: https://github.com/kubernetes/minikube/issues/12360#issuecomment-1430243861
+# TL;DR: 
+# 1. adding "volumesnapshots" and "csi-hostpath-driver" instead of  "default-storageclass" and "storage-provisioner"
+# 2. using "csi-hostpath-sc" as default storage class instead of "standard"
+
+resource "null_resource" "csi_hostpath_sc_patch" {
+  provisioner "local-exec" {
+    command = "kubectl patch storageclass csi-hostpath-sc -p '{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"true\"}}}'"
+  }
+  depends_on = [ minikube_cluster.docker ]
+}
+
+resource "null_resource" "standard_patch" {
+  provisioner "local-exec" {
+    command = "kubectl patch storageclass standard -p '{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"false\"}}}'"
+  }
+  depends_on = [ minikube_cluster.docker ]
 }
--- iac/main.tf
+++ iac/main.tf
@@ -10,10 +10,14 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.29.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.2"
+    }
   }
 }
 
 
 provider "minikube" {
-  kubernetes_version = "v1.28.3"
+  kubernetes_version = "v1.30.1"
 }
